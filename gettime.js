8017683aede981359e3f10f5282e76c6e642ab4e
wrontc13 <wrontc13@gmail.com>
2016-05-09 16:52:07 -0400

    Add files via upload

---
+++ gettime.js
@@ -0,0 +1,19 @@
+﻿function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
+
+var div = document.getElementById("txt")
+
+div.textContent = startTime()
+
+
